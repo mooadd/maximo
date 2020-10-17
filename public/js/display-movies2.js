ea4c7c6b1ae312bb47385ab2639a282cd19d5b1e
AbdiSalad <abdurahman.salad@hotmail.com>
2020-10-17 15:31:16 -0400

    You can now search for movies. However, isn't fetching the movies through our local server. Searches it through the the website

---
+++ public/js/display-movies2.js
@@ -0,0 +1,40 @@
+// http://www.omdbapi.com/?apikey=[yourkey]&
+const key = '9e8fdc9';
+
+$(document).ready(() => {
+  $('#searchForm').on('submit', (e) => {
+    let searchText = $('#searchText').val();
+    getMovies(searchText);
+    e.preventDefault();
+  });
+});
+
+function getMovies(searchText){
+  fetch(`http://www.omdbapi.com/?apikey=${key}&s=${searchText}`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      let movies = data.Search;
+      console.log(movies);
+      let output = '';
+
+      $.each(movies, (index, movie) => {
+        output += `
+          <div class="col-md-3">
+            <div class="well text-center">
+              <img src="${movie.Poster}">
+              <h5>${movie.Title}</h5>
+              <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href='#'>Movie Details</a>
+            </div>
+          </div>
+        `;
+      });
+
+      $('#movies').html(output);
+
+    })
+    .catch((err) =>{
+      console.log(err);
+    })
+}
