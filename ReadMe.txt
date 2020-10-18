6eeb29d71ee69f2cc53151451294aa79a736b450
AbdiSalad <abdurahman.salad@hotmail.com>
2020-10-18 13:21:18 -0400

    You can now sign in and register. TODO: profile page

---
+++ ReadMe.txt
@@ -0,0 +1,12 @@
+
+What to install:
+=> npm i --save express
+=> npm i -g nodemon
+=> npm i body-parser
+
+
+How to run:
+=> nodemon
+
+
+You simply just type 'nodemon'
--- app.js
+++ app.js
@@ -1,20 +1,72 @@
+const PORT = 9000
 const express = require('express')
 const path = require('path')
 const app = express()
-const PORT = 9000
+const bodyParser = require('body-parser');
+
+// An array to store user information
+let users = [];
+
+// Create application/json parser
+const jsonParser = bodyParser.json();
+
+// Create application/x-www-form-urlencoded parser
+const urlencodedParser = bodyParser.urlencoded({extended: false})
+
+// https://stackoverflow.com/questions/9177049/express-js-req-body-undefined
 
 app.use(express.static('public'))
 
 app.get('/Login', (req, res) => {
-    res.sendfile(__dirname + "/public/index.html");
+    res.sendFile(__dirname + "/public/index.html");
+})
+
+app.post('/Login', urlencodedParser, (req, res) => {
+  // now, lets check if a username and password is in an the array of users.
+  let bool = false;
+  try {
+    for (let i=0; i < users.length ; i++) {
+      if ((users[i].username === req.body.username) && (users[i].password === req.body.password)) {
+        console.log('found the user.')
+        bool = true;
+        res.send('temporary user page')
+        break;
+      }
+    }
+
+    // if the password or username was wrong, we bring them back here
+    if (bool === false) {
+      res.redirect('/Login')
+    }
+  } catch {
+    res.redirect('/Login')
+  }
 })
 
 app.get('/Register', (req, res) => {
-    res.sendfile(__dirname + "/public/sub-files/sign-up.html");
+    res.sendFile(__dirname + "/public/sub-files/sign-up.html");
+})
+
+app.post('/Register', urlencodedParser,  (req, res) => {
+  // will get a username, email, password
+  try {
+    users.push({
+      id: Date.now().toString(),
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    })
+    // If everything goes well. new user gets added to the array. we redirect
+    // the user to the login page
+    res.redirect('/Login')
+  } catch {
+    // If there is an error, we redirect the user back to the same page.
+    res.redirect('/Register')
+  }
 })
 
 app.get('/Reset-Password', (req, res) => {
-    res.sendfile(__dirname + "/public/sub-files/forgot-pass.html");
+    res.sendFile(__dirname + "/public/sub-files/forgot-pass.html");
 })
 
 
