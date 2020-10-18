09413035a1e19d7dfa09012d5af807e5948d8425
AbdiSalad <abdurahman.salad@hotmail.com>
2020-10-18 11:44:19 -0400

    Added some basic routing system

--- app.js
+++ app.js
@@ -5,6 +5,21 @@ const PORT = 9000
 
 app.use(express.static('public'))
 
+app.get('/Login', (req, res) => {
+    res.sendfile(__dirname + "/public/index.html");
+})
+
+app.get('/Register', (req, res) => {
+    res.sendfile(__dirname + "/public/sub-files/sign-up.html");
+})
+
+app.get('/Reset-Password', (req, res) => {
+    res.sendfile(__dirname + "/public/sub-files/forgot-pass.html");
+})
+
+
+
+
 app.listen(PORT, () => {
   console.log(`Server running at localhost${PORT}`)
 })
