77eb2ea158becde4ed39fb23e624e53fad8c7f6a
AbdiSalad <abdurahman.salad@hotmail.com>
2020-11-02 13:32:02 -0500

    I added some search menu. I plan to finish it by today (hopeful)

--- app.js
+++ app.js
@@ -1,90 +1,89 @@
-const PORT = 9000
-const express = require('express')
-const cors = require('cors')
-const path = require('path')
-const app = express()
-const bodyParser = require('body-parser')
-const expressLayouts = require('express-ejs-layouts');
+const PORT = 9000;
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const app = express();
+const bodyParser = require("body-parser");
+const expressLayouts = require("express-ejs-layouts");
 
 // An array to store user information
 let users = [];
 
 // Create application/json parser
 const jsonParser = bodyParser.json();
-
 // Create application/x-www-form-urlencoded parser
-const urlencodedParser = bodyParser.urlencoded({extended: false})
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-// https://stackoverflow.com/questions/9177049/express-js-req-body-undefined
-
-// app.use(bodyParser())
-app.use(cors())
+// THINGS WE NEED
+app.use(cors());
 app.use(expressLayouts);
-app.use(express.static('public'))
+app.use(express.static("public"));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-
-app.get('/Login', (req, res) => {
+// LOGIN ROUTE
+app
+  .route("/Login")
+  .get((req, res) => {
     res.sendFile(__dirname + "/public/index.html");
-})
-
-app.post('/Login', urlencodedParser, (req, res) => {
-  // now, lets check if a username and password is in an the array of users.
-  let bool = false;
-  try {
-    for (let i=0; i < users.length ; i++) {
-      if ((users[i].username === req.body.username) && (users[i].password === req.body.password)) {
-        console.log('found the user.')
-        bool = true;
-        res.render('about', {
-          username: req.body.username
-        })
-        break;
+  })
+  .post(urlencodedParser, (req, res) => {
+    // now, lets check if a username and password is in an the array of users.
+    let bool = false;
+    try {
+      for (let i = 0; i < users.length; i++) {
+        if (
+          users[i].username === req.body.username &&
+          users[i].password === req.body.password
+        ) {
+          console.log("found the user.");
+          bool = true;
+          res.render("about", {
+            username: req.body.username,
+          });
+          break;
+        }
       }
-    }
 
-    // if the password or username was wrong, we bring them back here
-    if (bool === false) {
-      res.redirect('/Login')
+      // if the password or username was wrong, we bring them back here
+      if (bool === false) {
+        res.redirect("/Login");
+      }
+    } catch {
+      res.redirect("/Login");
     }
-  } catch {
-    res.redirect('/Login')
-  }
-})
+  });
 
-app.get('/Register', (req, res) => {
+// REGISTER ROUTE
+app
+  .route("/Register")
+  .get((req, res) => {
     res.sendFile(__dirname + "/public/sub-files/sign-up.html");
-})
+  })
+  .post(urlencodedParser, (req, res) => {
+    // will get a username, email, password
+    try {
+      users.push({
+        id: Date.now().toString(),
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+      });
+      // If everything goes well. new user gets added to the array. we redirect
+      // the user to the login page
+      res.redirect("/Login");
+    } catch {
+      // If there is an error, we redirect the user back to the same page.
+      res.redirect("/Register");
+    }
+  });
 
-app.post('/Register', urlencodedParser,  (req, res) => {
-  // will get a username, email, password
-  try {
-    users.push({
-      id: Date.now().toString(),
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password
-    })
-    // If everything goes well. new user gets added to the array. we redirect
-    // the user to the login page
-    res.redirect('/Login')
-  } catch {
-    // If there is an error, we redirect the user back to the same page.
-    res.redirect('/Register')
-  }
-})
+// RESET PASSWORD ROUTE
+app.route("/Reset-Password").get((req, res) => {
+  res.sendFile(__dirname + "/public/sub-files/forgot-pass.html");
+});
 
-app.get('/Reset-Password', (req, res) => {
-    res.sendFile(__dirname + "/public/sub-files/forgot-pass.html");
-})
-
-
+// Runs the server on port 9000
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`)
-})
-
-// I have installed nodemon. to run, you simply type (nodemon) in the shell.
-// If you want to know more about nodemon, try searching it yourself.
-// Also, instead of running 'node app.js', I made it so that you can just type
-// 'npm start' to get the server to run.
+  console.log(`Server running at http://localhost:${PORT}`);
+});
---
+++ public/js/movie-search.js
@@ -0,0 +1,13 @@
+// These functions just change the dropdowns text.
+const changeToTitle = () => {
+  $("#dropdown-button").text("Title");
+};
+const changeToGenre = () => {
+  $("#dropdown-button").text("Genre");
+};
+const changeToYear = () => {
+  $("#dropdown-button").text("Year");
+};
+const changeToMinRating = () => {
+  $("#dropdown-button").text("Minrating (0-5)");
+};
